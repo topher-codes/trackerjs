144ad8b224832be15974b7177e565c708854be0a
Chris <ghost@drednaut.net>
2022-10-05 11:09:08 -0700

    initial commit
    Signed-off-by: Chris <ghost@drednaut.net>

---
+++ src/components/logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logo = () => {
+	return (
+		<div className="grid-logo">
+			<img src="./image001.png " alt="logo" />
+		</div>
+	);
+};
+
+export default Logo;
---
+++ src/hooks/firebase.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: 'AIzaSyDsPPEBHu6G-xQeM-nP5jZyPNwANn96s34',
+	authDomain: 'fir-project-48d1a.firebaseapp.com',
+	projectId: 'fir-project-48d1a',
+	storageBucket: 'fir-project-48d1a.appspot.com',
+	messagingSenderId: '812587000254',
+	appId: '1:812587000254:web:51857d1e3a2828b7d659ef',
+	measurementId: 'G-62MTBS55Z2',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const auth = getAuth(firebaseApp);
